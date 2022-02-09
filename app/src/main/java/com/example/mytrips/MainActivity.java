5e2e1a8fddf79146132de9bd0674f38b5d35ff1e
= <=>
2022-02-09 21:26:07 +0100

    DISEÑO GENERAL, TABS Y TOOLBAR HECHOS

--- app/src/main/java/com/example/mytrips/MainActivity.java
+++ app/src/main/java/com/example/mytrips/MainActivity.java
@@ -1,26 +1,69 @@
 package com.example.mytrips;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+import com.example.mytrips.adaptadorTabs.miAdaptador;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
+    public Toolbar myTB;
+    public TabLayout myTL;
+    public ViewPager2 myVP2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         main();
-
-
-
     }
 
     private void main() {
         controls();
+        initApp();
+    }
+
+    private void initApp() {
+        miAdaptador adapter = new miAdaptador(this);
+        myVP2.setAdapter(adapter);
+        new TabLayoutMediator(myTL,myVP2,(tab,position)->tab.setText(adapter.getTabTitle(position))).attach();
     }
 
     private void controls() {
+    myTB = findViewById(R.id.myToolbar);
+    setSupportActionBar(myTB);
+    myTL = findViewById(R.id.myTabs);
+    myVP2 = findViewById(R.id.myViewPager2);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.perfil) {
+            Toast.makeText(getApplicationContext(),"PRUEBA PERFIL TOOLBAR", Toast.LENGTH_LONG).show();
+
+        }
+        if (id == R.id.ajustes) {
+            Toast.makeText(getApplicationContext(),"PRUEBA AJUSTES TOOLBAR", Toast.LENGTH_LONG).show();
+        }
+        if (id == R.id.cerrarSesion) {
+            Toast.makeText(getApplicationContext(),"PRUEBA CIERRE SESION TOOLBAR", Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/mytrips/adaptadorTabs/miAdaptador.java
@@ -0,0 +1,59 @@
+package com.example.mytrips.adaptadorTabs;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import com.example.mytrips.fragments.misViajes;
+import com.example.mytrips.fragments.nuevoViaje;
+import com.example.mytrips.fragments.recomendaciones;
+
+public class miAdaptador extends FragmentStateAdapter {
+
+    public miAdaptador(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    public miAdaptador(@NonNull Fragment fragment) {
+        super(fragment);
+    }
+
+    public miAdaptador(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+       switch (position) {
+           case 0 : nuevoViaje nuevoviaje1 = new nuevoViaje();
+           return nuevoviaje1;
+           case 1 :
+               misViajes misViajes1 = new misViajes();
+               return misViajes1;
+           case 2 :
+               recomendaciones recomendaciones1 = new recomendaciones();
+               return recomendaciones1;
+           default : return null;
+       }
+    }
+
+    public CharSequence getTabTitle (int position) {
+        switch (position) {
+            case 0 : return "NUEVO VIAJE";
+            case 1: return "MIS VIAJES";
+            case 2: return "RECOMENDACIONES";
+            default : return "MyTrips";
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+
+
+
+}
